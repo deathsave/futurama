2fddd6def7625612e240bf3b4a1ba8d95f717dc1
Ryan <ryananthonyrichardson@gmail.com>
2024-11-28 12:35:47 -0800

    feat: mostly migrate config for 0.57.0

---
+++ tests/test_general.py
@@ -0,0 +1,20 @@
+"""Tests General/Base Functionality"""
+
+import os
+from mpf.tests.MpfMachineTestCase import MpfMachineTestCase
+
+class TestGeneral(MpfMachineTestCase):
+
+    """Tests start-up sequence"""
+
+    def get_config_file(self):
+        return 'config.yaml'
+
+    def get_machine_path(self):
+        return os.path.abspath(os.path.join(
+            os.path.realpath(__file__),
+            os.pardir,os.pardir
+        ))
+
+    def test_start(self):
+        self.assertModeRunning('attract')
