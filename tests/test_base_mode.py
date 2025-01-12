846e345581006a626bd6007088fde0c76227b936
Ryan <ryananthonyrichardson@gmail.com>
2025-01-12 11:41:11 -0800

    test: use MpfMachineTestCase for base test class

--- tests/test_base_mode.py
+++ tests/test_base_mode.py
@@ -1,11 +1,17 @@
-from mpf.tests.MpfGameTestCase import MpfGameTestCase
+from mpf.tests.MpfMachineTestCase import MpfMachineTestCase
 
-class TestBaseMode(MpfGameTestCase):
+class TestBaseMode(MpfMachineTestCase):
 
     def test_base_mode(self):
-        self.assertModeRunning("attract")
+        self._start_game()
+
+        # Test base mode integration here...
+
+    def _start_game(self):
         self.hit_and_release_switch("s_start")
         self.advance_time_and_run(1)
-        # TODO: this fails due to the game crashing
-        #       on the stepper config
-        self.assertModeRunning("game")
+        self.hit_and_release_switch("s_shooter_lane")
+        self.advance_time_and_run(1)
+        self.hit_and_release_switch("s_cap1")
+        self.advance_time_and_run(1)
+        self.assertEqual(1, self.machine.playfield.balls)
