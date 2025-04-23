98c341a75eea473220e7217671b3459bc8901a8b
Ryan <ryananthonyrichardson@gmail.com>
2025-04-23 00:27:03 -0700

    fix: try directly accessing tic wrapper

--- modes/attract/code/attract.py
+++ modes/attract/code/attract.py
@@ -1,8 +1,9 @@
+from mpf.platforms.pololu.pololu_ticcmd_wrapper import PololuTiccmdWrapper
 from mpf.modes.attract.code.attract import Attract
 
 class FuturamaAttract(Attract):
 
     def mode_start(self, **kwargs):
-        stepper = self.machine.steppers['zoidberg_stepper']
-        stepper.move_to_position(0)
+        tic = PololuTiccmdWrapper("00427235", self.machine)
+        tic._ticcmd("--halt-and-set-position 0")
         super().mode_start(**kwargs)
