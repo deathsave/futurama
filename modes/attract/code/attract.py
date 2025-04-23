998d7bae2fce7a207276ec45c979b67d01cb9d41
Ryan <ryananthonyrichardson@gmail.com>
2025-04-22 23:31:09 -0700

    fix: try to fix homing with cleaner interface

--- modes/attract/code/attract.py
+++ modes/attract/code/attract.py
@@ -1,14 +1,8 @@
-from mpf.core.mode import Mode
-from mpf.platforms.pololu import pololu_tic
-from mpf.platforms.pololu import pololu_ticcmd_wrapper
+from mpf.modes.attract.code.attract import Attract
 
-class Attract(Mode):
+class FuturamaAttract(Attract):
 
     def mode_start(self, **kwargs):
-        del kwargs
-        self._serial_number = "00427235"
-        pololu_tic.PololuTiccmdWrapper._run_subprocess_ticcmd(self, "--halt-and-set position 0")
-
-
-#previously tried:
-#self.platforms.pololu.pololu_ticcmd_wrapper._ticcmd(self, "--halt-and-set-position 0")
+        stepper = self.machine.steppers['zoidberg_stepper']
+        stepper.move_to_position(0)
+        super().mode_start(**kwargs)
