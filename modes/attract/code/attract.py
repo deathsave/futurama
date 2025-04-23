4b0b3a3baa1d3e27a08b831cf37abf2bbd1b7a3f
TheSpennyPoo <spencer.dixon.holmes@gmail.com>
2025-04-22 17:03:37 -0700

    software homing added but not active

--- modes/attract/code/attract.py
+++ modes/attract/code/attract.py
@@ -1,6 +1,14 @@
 from mpf.core.mode import Mode
+from mpf.platforms.pololu import pololu_tic
+from mpf.platforms.pololu import pololu_ticcmd_wrapper
 
 class Attract(Mode):
 
     def mode_start(self, **kwargs):
-        self.platforms.pololu.pololu_ticcmd_wrapper._ticcmd(self, "--halt-and-set-position, 0")
+        del kwargs
+        self._serial_number = "00427235"
+        pololu_tic.PololuTiccmdWrapper._run_subprocess_ticcmd(self, "--halt-and-set position 0")
+
+
+#previously tried:
+#self.platforms.pololu.pololu_ticcmd_wrapper._ticcmd(self, "--halt-and-set-position 0")
