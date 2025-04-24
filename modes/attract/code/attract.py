5c0d45c6fc5ab798ed2454499f864bd82561f6d0
TheSpennyPoo <spencer.dixon.holmes@gmail.com>
2025-04-23 22:02:28 -0700

    corrected syntax on smart_virtual if for zoidberg reset

--- modes/attract/code/attract.py
+++ modes/attract/code/attract.py
@@ -5,8 +5,8 @@ class FuturamaAttract(Attract):
 
     def mode_start(self, **kwargs):
 
-        if self.core.platform_controller._check_and_get_platform != "smart_virtual":
-            self._reset_tic()          
+        if self.machine.hardware_platforms['smart_virtual'] is None:
+            self._reset_tic()
         super().mode_start(**kwargs)
 
     def _reset_tic(self):
