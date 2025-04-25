b72a1701c234fb4d1ef0bdcf328ca22fce0b6f4f
Ryan <ryananthonyrichardson@gmail.com>
2025-04-25 16:38:48 -0700

    fix: try using event player to trigger tic reset

--- code/zoidberg.py
+++ code/zoidberg.py
@@ -4,12 +4,13 @@ from mpf.platforms.pololu.pololu_ticcmd_wrapper import PololuTiccmdWrapper
 class Zoidberg(CustomCode):
 
     def on_load(self):
-        self._reset_tic()
+        print("Loading Zoidberg tic homing reset")
+        self.machine.events.add_handler('reset_zoidberg_stepper', self.reset_tic)
 
-    def _reset_tic(self):
+    def reset_tic(self, **kwargs):
         if 'smart_virtual' in self.machine.hardware_platforms:
             print("Skipping Zoidberg tic homing reset")
         else:
+            print("Attempting Zoidberg tic homing reset")
             tic = PololuTiccmdWrapper("00427235", self.machine)
             tic.halt_and_set_position(0)
-            print("Zoidberg tic homing reset done")
