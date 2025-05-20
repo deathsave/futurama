d20c17c3ec4cae3f36ca2f23b95c7afa4fb7386a
TheSpennyPoo <spencer.dixon.holmes@gmail.com>
2025-05-20 00:53:52 -0700

    changed fuel gauge start state and requirements for moon delivery
    instead of starting at an empty tank but only needing 1/2 a tank for the moon delivery, you now start at half a tank but need a full tank for the moon delivery.  this being so that the "nibbler dark matter shot" light doesn't keep going during deliveries.

--- tests/test_moon_mode_success_fry_crate_no_extras.py
+++ tests/test_moon_mode_success_fry_crate_no_extras.py
@@ -55,7 +55,7 @@ class TestMoonModeSuccessFryCrateNoExtras(FullMachineTestCase):
         self.assertPlayerVarEqual(2, "fry_level")
         self.hit_and_release_switch("s_right_ramp")
         self.advance_time_and_run(3)
-        self.assertEqual("half", self.machine.state_machines.fuel_gauge_state.state)
+        self.assertEqual("full", self.machine.state_machines.fuel_gauge_state.state)
 
     def _start_moon_delivery(self):
         self.hit_switch_and_run("s_VUK", 5)
